492872b7b1078c5556a299592dcaac9a07218063
eitax <dm.kamaleev@gmail.com>
2023-04-11 20:20:42 +0300

    task

---
+++ scripts/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from wtforms import Form
+from wtforms import (StringField)
+from wtforms.validators import InputRequired, Length
+
+
+class LoginForm(Form):
+    username = StringField('Username:', validators=[InputRequired(), Length(min=5, max=30)])
+    password = StringField('Password:', validators=[InputRequired(), Length(min=5, max=30)])
+    email = StringField('Email:', validators=[Length(min=0, max=30)])
